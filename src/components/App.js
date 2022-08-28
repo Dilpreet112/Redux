862acb62ca23c171bcd2819d20ad59703cfe1161
Dilpreet Kaur <dhanjaldilpreet1@gmail.com>
2022-08-28 13:16:33 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+import Navbar from './Navbar';
+import Shop from './Shop';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Shop/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+        <h2>Deposit / Withdraw Money</h2>
+        <button className='btn btn-primary mx-2'>-</button>
+        Updated balance
+        <button className='btn btn-primary mx-2'>+</button>
+
+    </div>
+  )
+}
+
+export default Shop
